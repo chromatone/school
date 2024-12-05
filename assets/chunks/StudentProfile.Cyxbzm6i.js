50dc94b6c63a4b888efbfec994a728eaf8eb6ae8
davay42 <davay42@users.noreply.github.com>
2024-12-05 12:09:08 +0000

    deploy: f7b1244ee58f5b69521e8d51c5d9b7d18f3ddddb

--- assets/chunks/StudentProfile.Cyxbzm6i.js
+++
@@ -1,1 +0,0 @@
-import{u as r}from"./theme.Dv3McUTO.js";import{a,c as o,b as s,t as l,u as n}from"./framework.B8YXKKAO.js";const c={class:"flex flex-col gap-2"},_={__name:"StudentProfile",setup(u){const{user:t}=r();return(p,e)=>(a(),o("div",c,[e[0]||(e[0]=s("div",{class:"text-xl"},"USER",-1)),s("pre",null,l(n(t)),1)]))}};export{_ as default};
