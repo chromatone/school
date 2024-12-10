f77ce69e8b05defb1db1d92157a4a713b831ba6c
davay <davay@defucc.me>
2024-12-10 10:19:30 +0700

    feat: more stable load programs

--- programs/[program]/index.paths.js
+++ programs/[program]/index.paths.js
@@ -2,12 +2,18 @@ import { createDirectus, rest, readItems } from '@directus/sdk'
 
 export default {
   async paths() {
+    let programs = []
 
-    const programs = await createDirectus('https://schooldb.chromatone.center/').with(rest()).request(readItems('programs', {
-      fields: [
-        '*', 'courses.*', 'courses.teacher.*'
-      ]
-    }))
+    try {
+      programs = await createDirectus('https://schooldb.chromatone.center/').with(rest()).request(readItems('programs', {
+        fields: [
+          '*', 'courses.*', 'courses.teacher.*'
+        ]
+      }))
+    } catch (e) {
+      console.log('Programs paths not loaded')
+    }
+
 
     return programs.map(program => {
       let content = program.content
