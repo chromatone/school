e2f59e97cabbac27673b5f6887db816101f084d4
davay42 <davay42@users.noreply.github.com>
2024-12-23 15:13:47 +0000

    deploy: c94c5efafa1bdf61f81bb48284ee37f498a531e0

--- assets/chunks/useHash.BjdL9CFZ.js
+++
@@ -1,1 +0,0 @@
-import{r as a,d as o,a3 as s}from"./framework.LhDnjOZs.js";function t(){const e=a(""),n=()=>e.value=window.location.hash.slice(1);return o(()=>{n(),window.addEventListener("hashchange",n)}),s(()=>window.removeEventListener("hashchange",n)),e}export{t as u};
