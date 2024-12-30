b319007ea75960a0450ffae499363be9b9353623
davay42 <davay42@users.noreply.github.com>
2024-12-30 15:12:27 +0000

    deploy: 8b3177106586f4f9a716179fe199154cb8c01815

--- assets/chunks/useWS.C_IkFNlD.js
+++
@@ -1,1 +0,0 @@
-import{l as o,p as n,v as s}from"./theme.DUNlFEbY.js";import{r,o as a}from"./framework.DStf2H8E.js";const t=o("https://schooldb.chromatone.center/").with(n("ogbYm_VIY1zQ29J1BLd1Xf29EQnCjswk")).with(s()),e=r(!1);function f(){return a(async()=>{e.value||(await t.connect(),e.value=!0)}),{ws:t,connected:e}}export{f as u};
